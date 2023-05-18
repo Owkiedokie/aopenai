4db840b962c71f7103b1c33e510fb4fb06776ad1
Owkiedokie <86634778+Owkiedokie@users.noreply.github.com>
2023-05-18 11:48:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,54 +4,37 @@ import openai
 # Set up the OpenAI API
 openai.api_key = st.secrets["api_key"]
 
+# Title of the app
+st.title("C++ Code Generator")
+
+# User input for the programming task
+task = st.text_input("Enter the programming task:")
+
+# Generate C++ code using OpenAI's GPT-3.5 model
 def generate_code(task):
-    # Prepare the prompt
-    prompt = f"Task: {task}\n\n```cpp\n"
-    
-    # Generate the code using OpenAI's completions API
+    prompt = f"Task: {task}\n\n```cpp\n#include <iostream>\nusing namespace std;\n\nint main() {{\n\n}}\n```"
     response = openai.Completion.create(
-        engine="text-davinci-codex",
+        engine="text-davinci-003",
         prompt=prompt,
         max_tokens=100,
+        temperature=0.8,
         n=1,
         stop=None,
         temperature=0.8,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0,
-        best_of=1,
-        prompt_model=None,
-        log_level="info"
+        echo=True
     )
-    
-    # Extract the generated code from the API response
-    generated_code = response.choices[0].text
-    
-    return generated_code
+    code = response.choices[0].text.strip()
+    return code
 
-
-
-def main():
-    st.title("College Admission Chatbot")
-    st.write("Welcome to the college admission chatbot! Please ask your questions below:")
-    st.title("C++ Tutorial App")
-    st.header("Generate Code for Basic Programming Tasks")
-    
-    # User input
-    task = st.text_input("Enter the programming task:", "")
-    
-    if st.button("Generate Code"):
-        # Call OpenAI API to generate code based on the task
+# Generate and display the C++ code
+if st.button("Generate Code"):
+    if task:
         code = generate_code(task)
-        
-        # Display the generated code
-        st.code(code)
+        st.code(code, language="cpp")
+    else:
+        st.warning("Please enter a programming task.")
 
-def generate_code(task):
-    # OpenAI code generation logic goes here
-    # Use the task to generate the C++ code using OpenAI
-    
     return generated_code
-
-if __name__ == "__main__":
-    main()
