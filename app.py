d5c1d8f8b7ac075ff4ba6dc3303b8739087f7c25
Owkiedokie <86634778+Owkiedokie@users.noreply.github.com>
2023-05-26 19:22:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 import os
 
 # Retrieve OpenAI API key from environment variable
-api_key = os.getenv("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")
+api_key = os.getenv("OPENAI_API_KEY")
 st.write(api_key)  # Add this line to print the value of the API key
 if api_key is None:
     st.error("OpenAI API key not found. Please set it as a Streamlit Cloud environment variable.")
