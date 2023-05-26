4227613b22c434cf74e227c71b17a36bc6721a76
Owkiedokie <86634778+Owkiedokie@users.noreply.github.com>
2023-05-26 22:38:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import openai
+import requests
 from configparser import ConfigParser
 
 # Read API key from config file
@@ -11,6 +12,9 @@ api_key = config.get("openai", "api_key")
 if api_key is None:
     st.error("OpenAI API key not found. Please provide your API key in the config.ini file.")
     st.stop()
+
+# Define the API endpoint
+endpoint = "https://api.openai.com/v1/engines/davinci/completions"
 
 # Set up OpenAI API credentials
 openai.api_key = api_key
@@ -26,25 +30,32 @@ def main():
     if st.button("Translate"):
         with st.spinner("Translating..."):
             try:
-                # Call the OpenAI API to translate the text
-                response = openai.Completion.create(
-                    engine="davinci",
-                    prompt=f"{prompt}\n{text_input}\n-- Translation:",
-                    temperature=0.5,
-                    max_tokens=100,
-                    top_p=1.0,
-                    frequency_penalty=0.0,
-                    presence_penalty=0.0,
-                )
+                # Prepare the headers and payload
+                headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer {api_key}",
+                }
+                payload = {
+                    "prompt": f"{prompt}\n{text_input}\n-- Translation:",
+                    "temperature": 0.5,
+                    "max_tokens": 100,
+                    "top_p": 1.0,
+                    "frequency_penalty": 0.0,
+                    "presence_penalty": 0.0,
+                }
+
+                # Send the POST request to the OpenAI API
+                response = requests.post(endpoint, headers=headers, json=payload)
 
                 # Extract the translated text from the API response
-                translation = response.choices[0].text.strip().split("-- Translation:")[1]
+                data = response.json()
+                translation = data["choices"][0]["text"].strip().split("-- Translation:")[1]
 
                 st.success("Translation:")
                 st.write(translation)
 
-            except openai.error.AuthenticationError:
-                st.error("Authentication error. Please check your OpenAI API key.")
+            except requests.exceptions.RequestException as e:
+                st.error(f"An error occurred during the API request: {str(e)}")
             except Exception as e:
                 st.error(f"An error occurred: {str(e)}")
 
