92f2fc4fa544f45d39904e752db4a7fd91cb4ef3
Owkiedokie <86634778+Owkiedokie@users.noreply.github.com>
2023-05-26 18:14:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import openai
 
 # Set up the OpenAI API
-openai.api_key = st.secrets["api_key"]
+openai.api_key = st.secrets[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]
 
 # Title of the app
 st.title("C++ Code Generator")
